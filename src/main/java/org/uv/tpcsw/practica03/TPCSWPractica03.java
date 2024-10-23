04d03f42b2bc9ea639f9b53d35e3cdfdaf05bd87
usl311 <usiel311.ruiz@gmail.com>
2024-10-22 18:40:53 -0600

    Trabajo de clase

--- src/main/java/org/uv/tpcsw/practica03/TPCSWPractica03.java
+++ src/main/java/org/uv/tpcsw/practica03/TPCSWPractica03.java
@@ -1,5 +1,6 @@
 package org.uv.tpcsw.practica03;
 
+import java.util.List;
 import java.util.Set;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -19,20 +20,62 @@ public class TPCSWPractica03 {
 
         
             Empleado empleado = new Empleado();
-            empleado.setNombre("Eduardo");
-            empleado.setDireccion("Calle 20");
-            empleado.setTelefono("2322323");
+            empleado.setNombre("Evelia");
+            empleado.setDireccion("Calle 22");
+            empleado.setTelefono("2322344");
             empleado.setDepto(dep); 
 
             Set<Empleado> lstEmpleado = dep.getEmpleados();
             lstEmpleado.add(empleado);
-
-        
-            session.save(empleado);
+            
+                session.save(empleado);
 
             for (Empleado emp : lstEmpleado) {
                 System.out.println("Clave: " + emp.getClave() + " Nombre: " + emp.getNombre());
             }
+           
+//              DAOEmpleado daoEmpleado = new DAOEmpleado();
+//
+//                Long idEmpleado = empleado.getClave();
+//            Empleado empleadoEncontrado = daoEmpleado.findById(idEmpleado);
+//
+//            
+//            if (empleadoEncontrado != null) {
+//                System.out.println("Empleado encontrado: " + empleadoEncontrado.getNombre());
+//            } else {
+//                System.out.println("Empleado no encontrado.");
+//            }
+//            
+//              System.out.println("Lista de empleados del departamento:");
+//            for (Empleado emp : lstEmpleado) {
+//                System.out.println("Clave: " + emp.getClave() + " Nombre: " + emp.getNombre());
+//            }
+//
+//           
+//            System.out.println("\nLista de todos los empleados registrados:");
+//            List<Empleado> todosEmpleados = daoEmpleado.findAll();
+//
+//            if (todosEmpleados != null && !todosEmpleados.isEmpty()) {
+//                for (Empleado emp : todosEmpleados) {
+//                    System.out.println("ID: " + emp.getClave() + ", Nombre: " + emp.getNombre() +
+//                                       ", Dirección: " + emp.getDireccion() + ", Teléfono: " + emp.getTelefono());
+//                }
+//            } else {
+//                System.out.println("No hay empleados registrados.");
+//            }
+//        } else {
+//            System.out.println("Departamento no encontrado.");
+//        }
+//         Departamento departamento=new Departamento();
+//        departamento.setNombre("Administracion");
+//        SessionFactory sf=HibernateUtil.getSessionFactory();
+//        Session session=sf.getCurrentSession();
+//        Transaction t = session.beginTransaction();
+//        session.save(departamento);
+//        t.commit();
+//        
+        
+        
         }
 
         // Confirmar la transacción
